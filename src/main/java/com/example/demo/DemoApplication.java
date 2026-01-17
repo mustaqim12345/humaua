a11c873dacdc11611acabab6c78f214b18f8df05
mustaqim12345 <mustaqim8859@gmail.com>
2026-01-17 13:49:50 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -14,7 +14,7 @@ public class DemoApplication {
     }
 
     @GetMapping("/")
-    public String hello() {
-        return "Hello from Spring Boot + Jenkins + Docker 🚀";
+    public String home() {
+        return "Hello from Spring Boot!";
     }
 }
